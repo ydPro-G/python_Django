4852c4ddbf65af623851d46158f7221d1079ead9
ydPro-G <46178109+ydPro-G@users.noreply.github.com>
2020-10-12 14:51:40 +0800

    Update socket_server.py

--- learning_log/socket_server.py
+++ learning_log/socket_server.py
@@ -17,7 +17,7 @@ response = '\r\n'.join(response_params) # 响应数据，body内容长度+内容
 def handle_connection(conn,addr):
     request = b""
     while EOL1 not in request and EOL2 not in request:
-        request += conn.recv(1024)
+        request += conn.recv(1024) #从socket中读取字符
     print(request)
     conn.send(response.encode())  #response转为bytes后传输
     conn.close()
@@ -25,19 +25,19 @@ def handle_connection(conn,addr):
 def main():
     #socket.AF_INET用于服务器与服务器之间网络通信
     #socket.SOCK_STREAM用于基于TCP的流式socket通信
-    serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # 创建socket
     #设置端口可复用，保证我们每次按ctrl+c组合键后，快速重启
     serversocket.setsockopt(socket.SOL_SOCKET,socket.SOCK_STREAM, 1)
-    serversocket.bind(('127.0.0.1',8000))
-    serversocket.listen(5) #设置backlog--socket连接最大排队数量
+    serversocket.bind(('127.0.0.1',8000)) # 绑定端口号
+    serversocket.listen(5) #监听该端口号，最大排队数5
     print('http://127.0.0.1:8000')
 
     try:
         while True:
-            conn,address = serversocket.accept()
+            conn,address = serversocket.accept() # 允许客户端连接请求 
             handle_connection(conn, address) # 调用句柄连接函数
     finally:
-        serversocket.close()
+        serversocket.close() # 关闭
 
 if __name__ == '__main__':
-    main()
+    main() # 运行socket链接
