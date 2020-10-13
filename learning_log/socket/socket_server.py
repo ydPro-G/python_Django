bccf463dc4ede93852b035c745a992b56f6de396
ydPro-G <46178109+ydPro-G@users.noreply.github.com>
2020-10-13 11:44:25 +0800

    Update socket_server.py

--- learning_log/socket/socket_server.py
+++ learning_log/socket/socket_server.py
@@ -7,7 +7,7 @@ EOL2 = b'\n\r\n'
 
 
 
-body = '''<h2>Hello World!</h2><h1> 我们接下来学习django开发</h1>'''
+body = '''<h2>Hello World!</h2><h1> 我们接下来学习django开发</h1>'''# 响应体数据
 response_params = [  # 响应参数
     'HTTP/1.0 200 OK',   # 状态行，响应时的状态行
     'Date:Sat,18 apr 2020 01:01:01 GMT', #消息报头-时间
@@ -15,35 +15,38 @@ response_params = [  # 响应参数
     'Content-Length: {}\r\n', format(len(body.encode())), # 消息报头-内容长度
     body, #body也响应
 ]
-response = '\r\n'.join(response_params) # 响应数据，body内容长度+内容
+response = '\r\n'.join(response_params) # 返回的HTTP数据要求
 
 
 
 # conn 连接客户端，addr ip地址，端口
 def handle_connection(conn,addr):   # 接受请求数据，直到一个完整的http请求被接收完毕
-    request = b"" 
+    '''处理连接'''
+    request = b"" # 网络数据只能是byte型
+    # 判断有没有发送HTTP请求，该过程就是点击打印出的url，http请求由浏览器完成
+    # 如没有， 接受数据
     while EOL1 not in request and EOL2 not in request:
-        request += conn.recv(1024)  # 循环处理客户端请求
-    print(request)
-    conn.send(response.encode())  #response转为bytes后传输,发送回应
-    conn.close() #关闭
+        request += conn.recv(1024)  # 接受数据
+    print(request) # 打印出请求数据
+    conn.send(response.encode())  #以http格式的数据回复浏览器（客户端）
+    conn.close() #关闭连接
 
 
 
 def main(): # 运行函数
     #socket.AF_INET用于服务器与服务器之间网络通信
     #socket.SOCK_STREAM用于基于TCP的流式socket通信
-    serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # 创建服务器的连接
+    serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # 创建服务器的连接，实例化socket
     #设置端口可复用，保证我们每次按ctrl+c组合键后，快速重启，即使其他程序也在监听同样的端口
     serversocket.setsockopt(socket.SOL_SOCKET,socket.SOCK_STREAM, 1)
-    serversocket.bind(('127.0.0.1',8000)) # 绑定端口号
+    serversocket.bind(('127.0.0.1',8000)) # 绑定ip 与port
     serversocket.listen(5) #监听该端口号，响应从客户端过来的连接请求
-    print('http://127.0.0.1:8000')
+    print('http://127.0.0.1:8000') # 控制台带url
 
     try:
         while True:
-            conn,address = serversocket.accept() # 允许客户端连接请求 
-            handle_connection(conn, address) # 调用句柄连接函数
+            conn,address = serversocket.accept() # 等待客户端建立连接 
+            handle_connection(conn, address) # 调用句柄连接函数，处理客户端请求
     finally:
         serversocket.close() # 关闭
 
