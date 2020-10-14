8749be561034be4b609b469b4ed8136479fe0b92
ydPro-G <46178109+ydPro-G@users.noreply.github.com>
2020-10-14 09:39:01 +0800

    删除

--- learning_log/learning_logs/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from learning_logs.models import Topic,Entry  # 导入模型
-
-# 注册模型，这些模型都是models里面的类
-admin.site.register(Topic) # 注册模型Topic
-admin.site.register(Entry) # 注册模型Entry
-
-
